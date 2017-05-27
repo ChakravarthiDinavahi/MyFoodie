9d6fbf929b231a8612f6bec5b6c28014ee21afff
chakri <chakravarthi.dinavahi@gmail.com>
2017-05-27 19:04:04 +0530

    Added code for restaurent controller, item controller and set view for login

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -1,2 +1,23 @@
 class RestaurantController < ApplicationController
+  def index
+    @restaurants=Restaurent.all
+  end
+  def new
+  end
+  def create
+    @restaurant=Restaurent.new
+    @restaurant.name=params[:name]
+    @restaurant.save
+    @restaurant.addresses.create(:location =>params[:address] )
+  end
+  def edit
+  end
+  def update
+  end
+  def show
+  end
+  def destroy
+  end
+
+
 end
